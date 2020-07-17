ecc7532f0aae7efedee196fe242405dcd51f08ca
avivmachrus <avivmachrus@gmail.com>
2020-07-17 09:43:21 +0700

    test material-ui components

--- starter/pages/about.js
+++ starter/pages/about.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
 
 const About = () => {
   return (
@@ -10,6 +12,11 @@ const About = () => {
           <a>Home</a>
         </Link>
       </h3>
+      <Avatar
+        alt="avatar-img"
+        src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/12/129ffc8bfee7ba994b86ca7a930d1a7a48383523_full.jpg"
+      />
+      <Chip label="mankind" />
     </div>
   );
 };
